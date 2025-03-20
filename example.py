a992dce57bc7f8aa52d519455c5940413ccba2db
Soumen Bhuin <smbhuin@live.com>
2025-03-20 10:25:40 +0530

    feat: added example

---
+++ example.py
@@ -0,0 +1,29 @@
+from openai import OpenAI
+
+client = OpenAI(api_key="demo", base_url="http://localhost:8000/v1")
+
+def chat_completion():
+    completion = client.chat.completions.create(
+    model="mistral-7b",
+    messages=[
+        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": "Hello!"}
+    ]
+    )
+
+    print(completion.choices[0].message.content)
+
+def image_generation():
+    response = client.images.generate(
+        model="dreamshaper-8",
+        prompt="portrait photo of muscular bearded guy in a worn mech suit, light bokeh, intricate, steel metal, elegant, sharp focus, soft lighting, vibrant colors",
+        size="512x512",
+        quality="standard",
+        n=1,
+    )
+
+    print(response.data[0].url)
+
+
+# chat_completion()
+image_generation()
